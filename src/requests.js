a1b39932b24a65f2bbd0c09d588e5530688380c5
lensk65 <lens_112008@yahoo.com>
2021-06-23 17:40:37 -0700

    netflix

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "3a6d9b1ec661de1f27c8621f1c4f65ee";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
